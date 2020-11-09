9d3154cf6804d58590505116c8869be1bdebc605
JaniceMonicaPangau <69891923+JaniceMonicaPangau@users.noreply.github.com>
2020-11-09 20:34:10 +0800

    Menghitung BMI menggunakan JavaScript

---
+++ Pertemuan_19/index.js
@@ -0,0 +1,50 @@
+//variabel
+
+// let & cons
+
+// let _umur = 25;
+// _umur = 26;
+// // let nilai = 95.5;
+// // let sudahMenikah = false;
+// // let $nama = "John Doe";
+// // console.log(_umur);
+// // console.log($nama);
+
+//     let alamat;
+//     console.log(alamat);
+
+// // const tahunLahir = 2010;
+// // console.log(tahunLahir);
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("Nama Saya" + namaDepan + " " namaBelakang " " + umur " ");
+
+//ternary operator
+// let angka1 = 10;
+// let angka2 = 9;
+
+// console.log(angka1 <= angka2);
+
+// //operator logika
+// let kondisi1 = 1<2;
+// let kondisi2 = true;
+
+// console.log(kondisi1 || kondisi2);
+// console.log(!log)
+
+//let angka = prompt("Masukkan angka : ");
+// console.log("angka yang di masukkan adalah" + angka);
+
+//          Exersice 7 
+let berat = prompt("Masukkan berat badan anda (kg) =  ");
+let tinggi = prompt("Masukkan tinggi badan Anda (cm) = ");
+let bni = berat / (tinggi * tinggi);
+
+
+console.log ( "Your BNI =  " + bni);
